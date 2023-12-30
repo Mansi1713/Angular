b4c5be05dcc74a45a00bc877d18e3e6a2db204b8
Mansi Mahesh Kaleshwarkar <66161381+Mansi1713@users.noreply.github.com>
2023-12-30 22:43:20 +0530

    Add files via upload

---
+++ Assignment No 6/Assignmnet6-4/src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-comp.component.html',
+  styleUrl: './new-comp.component.css'
+})
+export class NewCompComponent {
+
+    public name:String="Marvellous"
+
+    public name1:String="Infosystems"
+
+}
